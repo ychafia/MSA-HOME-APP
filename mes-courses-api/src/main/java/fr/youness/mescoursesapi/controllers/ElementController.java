407f548c5d81256572a38a28df74360c4eb4c457
ychafia <chafiayouness@gmail.com>
2019-10-15 12:13:22 +0200

    Should call zuul Gateway to request mes-courses-api

--- mes-courses-api/src/main/java/fr/youness/mescoursesapi/controllers/ElementController.java
+++ mes-courses-api/src/main/java/fr/youness/mescoursesapi/controllers/ElementController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/elementsapi")
+@RequestMapping("mes-courses-api")
 public class ElementController {
 
     @GetMapping(value = "/elements")
