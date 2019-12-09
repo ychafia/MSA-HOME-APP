d0420c300b5622bc2471949b43bd38da557f41b3
unknown <chafiayouness@gmail.com>
2019-12-09 19:13:52 +0100

    update data.sql : fix bug duplicate primary key

--- mes-notes-api/src/main/resources/data.sql
+++ mes-notes-api/src/main/resources/data.sql
@@ -1,5 +1,3 @@
 ALTER TABLE `note` CHANGE `description_note` `description_note` LONGTEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
 
-INSERT INTO `note` (`id_note`, `completed_note`, `description_note`, `recall_date_note`, `title_note`, `updated_timestamp`)
-VALUES ('4', b'0', 'sscsc', '2019-11-20 00:00:00', 'csscs', '2019-11-20 00:00:00');
 
